64ef4f0348002a89baa5685b12885e579e6f3a90
Diego Rodríguez <99515379+DiegoR-99@users.noreply.github.com>
2022-02-17 14:54:38 -0600

    Label
    Agregué un label

--- Factorial/Factorial/Form1.Designer.vb
+++ Factorial/Factorial/Form1.Designer.vb
@@ -24,11 +24,12 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(343, 155)
+        Me.Button1.Location = New System.Drawing.Point(352, 155)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(88, 27)
         Me.Button1.TabIndex = 0
@@ -37,16 +38,27 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(227, 108)
+        Me.TextBox1.Location = New System.Drawing.Point(250, 106)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(304, 23)
         Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tw Cen MT", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(125, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(581, 40)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Calculadora de Factoriales 201708957"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
@@ -58,4 +70,5 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
 End Class
