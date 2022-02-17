e07c07db29fecf02bdf886c3c6e748ea06dc0a12
Diego Rodríguez <99515379+DiegoR-99@users.noreply.github.com>
2022-02-17 15:19:04 -0600

    Editando el código
    Empecé a editar el código

--- Factorial/Factorial/Form1.Designer.vb
+++ Factorial/Factorial/Form1.Designer.vb
@@ -25,6 +25,8 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -53,11 +55,33 @@ Partial Class Form1
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Calculadora de Factoriales 201708957"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tw Cen MT", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(174, 213)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(171, 40)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Resultado:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tw Cen MT", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(560, 213)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 40)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "..."
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
@@ -71,4 +95,6 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
--- Factorial/Factorial/Form1.vb
+++ Factorial/Factorial/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+        'formula para calculo n! = 1 x 2 x 3 x 4 x 5 x ... x (n-1) x n
+
+    End Sub
 End Class
