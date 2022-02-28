5e34845c4fdfc1d27fe9e06e9d91e785bd6e6ceb
Diego Rodríguez <99515379+DiegoR-99@users.noreply.github.com>
2022-02-27 18:03:33 -0600

    Finalizado

--- Tarea 5/Tarea 5/Program.vb
+++ Tarea 5/Tarea 5/Program.vb
@@ -2,27 +2,55 @@ Imports System
 
 Module Program
     Sub Main()
-        Dim i As Double
-        Dim j As Double
-        Dim n As Double
+        Dim x As Double
+        Dim factorial As Double
+        Dim numero As Double
         Dim Esperar As Char
         While Esperar <> "z" And Esperar <> "Z"
             Try
-                j = 1
+                factorial = 1
                 Console.Clear()
                 'formula para calculo n! = 1 x 2 x 3 x 4 x 5 x ... x (n-1) x n
                 Console.Write("Ingresar un numero entero positivo para calcular el factorial:")
                 Console.WriteLine()
                 Console.WriteLine()
-                n = Console.ReadLine()
-                For i = 1 To n Step 1
-                    j = j * i
-                Next
-                Console.WriteLine()
-                Console.Write("El factorial de " & n & " es = ")
-                Console.WriteLine()
-                Console.WriteLine()
-                Console.WriteLine(j)
+                numero = Console.ReadLine()
+
+                If numero < 0 Then
+                    Console.WriteLine()
+                    Console.WriteLine("Solamente se permite el 0 y enteros positvos")
+
+                ElseIf numero >= 0 Then
+                    For x = 1 To numero Step 1
+                        factorial = factorial * x
+                    Next
+                    Console.WriteLine()
+                    Console.Write("El factorial de " & numero & " es = ")
+                    Console.WriteLine()
+                    Console.WriteLine()
+                    Console.WriteLine(factorial)
+                    Console.WriteLine()
+                End If
+
+                If (factorial > 1) Then
+                    Dim indice = 1
+                    Dim cantidad As Integer = 0
+                    Dim resultado As String = ""
+                    While (indice <= factorial)
+                        If (indice Mod 4 = 0) Then
+                            resultado = resultado + indice.ToString + ","
+                            cantidad = cantidad + 1
+                        End If
+                        indice = indice + 1
+                    End While
+                    Console.WriteLine("Numeros multiplos de 4 entre 0 y " & factorial & " = ")
+                    Console.WriteLine()
+                    Console.WriteLine(resultado)
+                    Console.WriteLine()
+                    Console.WriteLine("La cantidad de numeros multiplos de 4 entre 0 y " & factorial & " es de = ")
+                    Console.WriteLine()
+                    Console.WriteLine(cantidad.ToString)
+                End If
             Catch ex As Exception
                 Console.WriteLine(ex.Message)
             End Try
