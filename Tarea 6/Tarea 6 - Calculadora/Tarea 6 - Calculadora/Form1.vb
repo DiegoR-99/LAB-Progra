d8e460a7d3ff087d7cb44a2c4a396ae34da0f344
Diego Rodríguez <99515379+DiegoR-99@users.noreply.github.com>
2022-03-03 17:43:16 -0600

    Finalizado

--- Tarea 6/Tarea 6 - Calculadora/Tarea 6 - Calculadora/Form1.vb
+++ Tarea 6/Tarea 6 - Calculadora/Tarea 6 - Calculadora/Form1.vb
@@ -33,14 +33,11 @@
         numeroEntrante = Aumentar(numeroEntrante)
         Label4.Text = numeroEntrante
     End Sub
-
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
         Dim txtA = Label2.Text
         Dim numeroA = Int32.Parse(txtA)
-
         Dim txtB = Label4.Text
         Dim numeroB = Int32.Parse(txtB)
-
         Dim resultadoSuma = Suma(numeroA, numeroB)
         'MsgBox(resultadoSuma)
         Label5.Text = "EL RESULTADO DE A + B ES " & resultadoSuma.ToString
@@ -50,14 +47,11 @@
         resultado = numeroA + numerob
         Return resultado
     End Function
-
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
         Dim txtA = Label2.Text
         Dim numeroA = Int32.Parse(txtA)
-
         Dim txtB = Label4.Text
         Dim numeroB = Int32.Parse(txtB)
-
         Dim resultadoResta = Resta(numeroA, numeroB)
         Label5.Text = "EL RESULTADO DE A - B ES " & resultadoResta.ToString
     End Sub
@@ -66,14 +60,11 @@
         resultado = numeroA - numerob
         Return resultado
     End Function
-
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
         Dim txtA = Label2.Text
         Dim numeroA = Int32.Parse(txtA)
-
         Dim txtB = Label4.Text
         Dim numeroB = Int32.Parse(txtB)
-
         Dim resultadoMultiplicacion = Multiplicacion(numeroA, numeroB)
         Label5.Text = "EL RESULTADO DE A * B ES " & resultadoMultiplicacion.ToString
     End Sub
@@ -82,28 +73,24 @@
         resultado = numeroA * numerob
         Return resultado
     End Function
-
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
         Dim txtA = Label2.Text
         Dim numeroA = Int32.Parse(txtA)
-
         Dim txtB = Label4.Text
         Dim numeroB = Int32.Parse(txtB)
-
         Dim resultadoDivision = Division(numeroA, numeroB)
         Label5.Text = "EL RESULTADO DE A / B ES " & resultadoDivision.ToString
     End Sub
-    Function Division(numeroA As Integer, numerob As Integer) As Integer
-        Dim resultado As Integer
+    Function Division(numeroA As Integer, numerob As Integer) As Double
+        Dim resultado As Double
         resultado = numeroA / numerob
         Return resultado
     End Function
     Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
-        Clear.Label2.Text
-        Label4.Text = ""
-        Label5.Text = ""
+        Label2.Text = "0"
+        Label4.Text = "0"
+        Label5.Text = "EL RESULTADO ES:"
     End Sub
-
     Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
         Me.Close()
         If (True) Then
